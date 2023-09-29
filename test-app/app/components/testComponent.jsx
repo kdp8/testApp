2f86db74aa12082fc6e29deaa775bc5083ffb18b
Krishang <kdp8@njit.edu>
2023-09-29 01:50:56 -0400

    Added NJIT logo as a test

---
+++ test-app/app/components/testComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SmallImage = () => {
+  return (
+    <div className="image-container">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/New_Jersey_IT_seal.svg/1200px-New_Jersey_IT_seal.svg.png"
+        alt="Small Image"
+        className="small-image"
+      />
+    </div>
+  );
+};
+
+export default SmallImage;
